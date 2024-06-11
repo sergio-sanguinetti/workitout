f79f9fe43dd584491e1f19491b99bde36ace65a5
Sergio Sanguinetti <59840657+sergio-sanguinetti@users.noreply.github.com>
2024-06-10 21:25:42 -0500

    cambio 2

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,23 +7,23 @@ import Sidebar from "./components/sidebar";
 import { SessionProvider } from 'next-auth/react';
 const inter = Inter({ subsets: ["latin"] });
 
-// export const metadata = {
-//   title: "WORKITOUT",
-//   description: "Plataforma de servicios",
-//   generator: "Next.js",
-//   manifest: "/manifest.json",
-//   keywords: ["nextjs", "nextjs13", "next13", "pwa", "next-pwa"],
-//   themeColor: [{ media: "(prefers-color-scheme: dark)", color: "#fff" }],
-//   authors: [
-//     { name: "Sergio Sanguinetti" },
+export const metadata = {
+  title: "WORKITOUT",
+  description: "Plataforma de servicios",
+  generator: "Next.js",
+  manifest: "/manifest.json",
+  keywords: ["nextjs", "nextjs13", "next13", "pwa", "next-pwa"],
+  themeColor: [{ media: "(prefers-color-scheme: dark)", color: "#fff" }],
+  authors: [
+    { name: "Sergio Sanguinetti" },
    
-//   ],
-//   viewport: "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
-//   icons: [
-//     { rel: "apple-touch-icon", url: "icons/icon-128x128.png" },
-//     { rel: "icon", url: "icons/icon-128x128.png" },
-//   ],
-// };
+  ],
+  viewport: "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
+  icons: [
+    { rel: "apple-touch-icon", url: "icons/icon-128x128.png" },
+    { rel: "icon", url: "icons/icon-128x128.png" },
+  ],
+};
 
 export default function RootLayout({ children }) {
   return (
