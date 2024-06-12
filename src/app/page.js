85ffdb7fef331d667cb5e0dc6074f5a1034f39f6
Sergio Sanguinetti <59840657+sergio-sanguinetti@users.noreply.github.com>
2024-06-12 16:03:22 -0500

    correcion login

--- src/app/page.js
+++ src/app/page.js
@@ -5,39 +5,53 @@ import "react-toastify/dist/ReactToastify.css";
 import { DOMAIN_BACK, DOMAIN_FRONT } from '../../env';
 import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
 import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
-import { signIn, signOut, useSession } from 'next-auth/react';
-
 
 export default function Login() {
-
-  const { data: session } = useSession();
-
   const [correo, setCorreo] = useState('');
   const [contraseña, setContraseña] = useState('');
   const [showPassword, setShowPassword] = useState(false);
 
   /**@async funcion para logear a la plataforma */
-  const Logearse = () => {
-    console.log(correo);
-    console.log(contraseña);
-
+  const Logearse = async () => {
     const regexCorreo = /^[\w.-]+@[a-zA-Z\d.-]+\.[a-zA-Z]{2,}$/;
 
     if (regexCorreo.test(correo) && contraseña.length > 0) {
-      if (correo === "utp@utp.edu.pe" && contraseña === "Contraseña1!") {
-        toast.success("Redirigiendo a la plataforma!", {
+      const formulario = new FormData();
+      formulario.append('email', correo);
+      formulario.append('password', contraseña);
+
+      try {
+        const response = await fetch(`${DOMAIN_BACK}?tabla=acceder_sistemaWORK`, {
+          method: 'POST',
+          body: formulario,
+        });
+     
+        const data = await response.json();
+
+        console.log(data);
+
+        if (data.estado === 1) {
+          toast.success(data.mensaje, {
+            position: "top-right"
+          });
+
+          setTimeout(() => {
+            window.location.href = DOMAIN_FRONT + 'plataforma';
+          }, 2000);
+
+        } else {
+          toast.error(data.mensaje, {
+            position: "top-right"
+          });
+        }
+      } catch (error) {
+        toast.error('Error al logearse: ' + error.message, {
           position: "top-right"
         });
-        setTimeout(() => {
-          window.location.href = DOMAIN_FRONT + 'plataforma';
-        }, 2000);
-      } else {
-        toast.error("Correo o Contraseña Incorrectos!", {
-          position: "top-center"
-        });
       }
+    
     } else {
-      toast.error("Ingrese los datos correctamente!", {
+      toast.error("Correo o Contraseña Incorrectos!", {
         position: "top-center"
       });
     }
@@ -50,58 +64,49 @@ export default function Login() {
   return (
     <>
       <ToastContainer />
-      <section class="ftco-section">
-        <div class="container">
-          <div class="row justify-content-center">
-            <div class="col-md-6 text-center mb-5"></div>
+      <section className="ftco-section">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-md-6 text-center mb-5"></div>
           </div>
-          <div class="row justify-content-center">
-            <div class="col-md-7 col-lg-5">
-              <div class="wrap">
+          <div className="row justify-content-center">
+            <div className="col-md-7 col-lg-5">
+              <div className="wrap">
                 <div className="img" style={{ backgroundImage: 'url("/login/bg-1.jpg")' }}></div>
-                <div class="login-wrap p-4 p-md-5">
-                  <div class="">
-                    <div class="text-center">
+                <div className="login-wrap p-4 p-md-5">
+                  <div className="">
+                    <div className="text-center">
                       <center>
-                        <h3 class="mb-4">WorkItOut</h3>
+                        <h3 className="mb-4">WorkItOut</h3>
                       </center>
                     </div>
-                    {/* <div class="w-100">
-                      <p class="social-media d-flex justify-content-end">
-                        <a href="#" class="social-icon d-flex align-items-center justify-content-center"><span class="fa fa-facebook"></span></a>
-                        <a href="#" class="social-icon d-flex align-items-center justify-content-center"><span class="fa fa-twitter"></span></a>
-                      </p>
-                    </div> */}
                   </div>
-                  <form action="#" class="">
-                    <div class="form-group mt-3">
-                      <label style={{ color: '#000' }} for="username">Correo Electrónico</label>
-                      <input type="mail" class="form-control p-2" value={correo} onChange={(e) => setCorreo(e.target.value)} required />
+                  <form action="#" className="">
+                    <div className="form-group mt-3">
+                      <label style={{ color: '#000' }} htmlFor="username">Correo Electrónico</label>
+                      <input type="mail" className="form-control p-2" value={correo} onChange={(e) => setCorreo(e.target.value)} required />
                     </div>
-                    <div class="form-group mt-3">
-                      <label style={{ color: '#000' }} class="" for="password">Contraseña</label>
-                      <div class="input-group">
-                        <input id="password-field" type={showPassword ? "text" : "password"} class="form-control p-2" value={contraseña} onChange={(e) => setContraseña(e.target.value)} required />
-                        <div class="input-group-append">
-                          <button type="button" class="btn btn-outline-secondary" onClick={toggleShowPassword}>
+                    <div className="form-group mt-3">
+                      <label style={{ color: '#000' }} className="" htmlFor="password">Contraseña</label>
+                      <div className="input-group">
+                        <input id="password-field" type={showPassword ? "text" : "password"} className="form-control p-2" value={contraseña} onChange={(e) => setContraseña(e.target.value)} required />
+                        <div className="input-group-append">
+                          <button type="button" className="btn btn-outline-secondary" onClick={toggleShowPassword}>
                             {showPassword ? <VisibilityOffIcon /> : <RemoveRedEyeIcon />}
                           </button>
                         </div>
                       </div>
                     </div>
-                    <div class="form-group">
-                      <button type="button" class="form-control btn btn-primary rounded submit px-3" onClick={Logearse}>INGRESAR</button>
+                    <div className="form-group">
+                      <button type="button" className="form-control btn btn-primary rounded submit px-3" onClick={Logearse}>INGRESAR</button>
                     </div>
-                    <div class="form-group d-md-flex">
-                      <div class="w-50 text-md-left">
+                    <div className="form-group d-md-flex">
+                      <div className="w-50 text-md-left">
                         <a href="#" style={{ color: '#01d28e' }}>Te olvidaste la contraseña?</a>
                       </div>
                     </div>
                   </form>
                   <p className="text-center">Aún no te registras? <a data-toggle="tab" href={DOMAIN_FRONT+'registro'}>REGISTRARSE</a></p>
-                  <div class="form-group">
-                      <button type="button" class="form-control btn btn-primary rounded submit px-3" onClick={()=> signIn()}>GOOGLE</button>
-                    </div>
                 </div>
               </div>
             </div>
